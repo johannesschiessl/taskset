efd61c9fa4867734e00114d4cd8c2212d6243436
Johannes <150372753+johannesschiessl@users.noreply.github.com>
2025-09-03 08:20:33 +0200

    auth

---
+++ convex/auth.ts
@@ -0,0 +1,32 @@
+import {
+  BetterAuth,
+  type AuthFunctions,
+  type PublicAuthFunctions,
+} from "@convex-dev/better-auth";
+import { api, components, internal } from "./_generated/api";
+import { query } from "./_generated/server";
+import type { Id, DataModel } from "./_generated/dataModel";
+
+const authFunctions: AuthFunctions = internal.auth;
+const publicAuthFunctions: PublicAuthFunctions = api.auth;
+
+export const betterAuthComponent = new BetterAuth(components.betterAuth, {
+  authFunctions,
+  publicAuthFunctions,
+});
+
+export const {
+  createUser,
+  updateUser,
+  deleteUser,
+  createSession,
+  isAuthenticated,
+} = betterAuthComponent.createAuthFunctions<DataModel>({
+  onCreateUser: async (ctx, user) => {
+    return ctx.db.insert("users", {});
+  },
+
+  onDeleteUser: async (ctx, userId) => {
+    await ctx.db.delete(userId as Id<"users">);
+  },
+});
